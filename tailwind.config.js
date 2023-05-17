14b5374cebeeb3327accc5e9a3ed57c80e689059
javad <jwadfthy2@gmail.com>
2023-05-17 11:37:18 +0330

    import tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/App.vue","./src/components/**/*.vue", "./src/views/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
